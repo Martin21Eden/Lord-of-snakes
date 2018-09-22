51d4b9326bceb1614fc45a43542c01fda157a61b
Martin21Eden <martin21eden@gmail.com>
2018-09-22 17:56:23 +0300

    arguments

--- Parser/README.md
+++ Parser/README.md
@@ -1,3 +1,4 @@
-1. For starting scripts you have to install requirements.
+1. For starting scripts you have to install dependency from requirements.
 2. Also install drivers: https://selenium-python.readthedocs.io/installation.html#drivers
-3. By default, the installed browser is Firefox, but you can change it on Chrome in the script.
+3. bash: python3 parser.py -c 'Juzgado de Letras del Trabajo de Arica', '' --> required.
+4. By default, the installed browser is Firefox, but you can change it on Chrome in the script.
--- Parser/parser.py
+++ Parser/parser.py
@@ -6,6 +6,7 @@ import csv
 from bs4 import BeautifulSoup
 from functools import wraps
 import logging
+import sys, getopt
 
 
 def my_logger(orig_funk):
@@ -29,6 +30,9 @@ class Parser:
         self.css_selector = '.TESTcpBorder > tbody:nth-child(1) > tr:nth-child(1) > td:nth-child(1) > center:nth-child(1)' \
                             ' > table:nth-child(2) > tbody:nth-child(1) > tr:nth-child(2) > td:nth-child(5)'
         self.data = []
+        self.list_courts = []
+        self.dict_enum_courts = None
+        self.court_name = None
         self.driver = webdriver.Firefox()
         """When starting Chrome"""
         # chrome_options = webdriver.ChromeOptions()
@@ -36,6 +40,14 @@ class Parser:
         # chrome_options.add_argument('--headless')
         # chrome_options.add_argument('--disable-gpu')
         # self.driver = webdriver.Chrome(chrome_options=chrome_options)
+
+    @my_logger
+    def create_list_dict_courts(self, html):
+        """Create list and dict of courts from html"""
+        soup = BeautifulSoup(html.get_attribute('innerHTML'), features="html.parser")
+        self.list_courts = [court.text.strip() for court in soup.find_all('option')]
+        new_dict_enum = dict(enumerate(self.list_courts, start=1))
+        self.dict_enum_courts = {value: key for key, value in new_dict_enum.items()}
 
     @property
     @my_logger
@@ -48,9 +60,17 @@ class Parser:
         self.driver.switch_to.frame(frame)
         WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable((By.ID, 'tdDos'))).click()
         WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="IMG_FEC_Desde"]'))).click()
-        WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, self.css_selector))).click()
+        WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, self.css_selector))).click()  # choose date
+        html = WebDriverWait(self.driver, 10).until(
+            EC.presence_of_element_located((By.XPATH, '//*[@id="COD_TribunalSinTodos"]')))
+        self.create_list_dict_courts(html)
+        try:
+            WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable(
+                (By.XPATH, f'/html/body/form/table[6]/tbody/tr/td[1]/div[2]/select/option[{self.dict_enum_courts[self.court_name]}]'))).click()
+        except:
+            sys.exit("Please enter correct court name!")
         WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable(
-            (By.XPATH, '/html/body/form/table[6]/tbody/tr/td[2]/img'))).click()
+            (By.XPATH, '/html/body/form/table[6]/tbody/tr/td[2]/img'))).click()  # click button
         return WebDriverWait(self.driver, 10).until(
                 EC.presence_of_element_located((By.XPATH, '/html/body/form/table[8]')))
 
@@ -67,19 +87,37 @@ class Parser:
 
     @staticmethod
     @my_logger
-    def save_csv(data):
+    def save_csv(data, file_name='data'):
         """Save data in csv"""
-        with open('./data/data.csv', 'w', newline='', encoding='utf-8') as csvfile:
+        with open(f'./data/{file_name}.csv', 'w', newline='', encoding='utf-8') as csvfile:
             csv_writer = csv.writer(csvfile, delimiter='\n',
                                     quotechar='|', quoting=csv.QUOTE_MINIMAL)
             csv_writer.writerow(data)
 
 
-if __name__ == '__main__':
+def main(argv):
+    court = None
+    try:
+        opts, args = getopt.getopt(argv, "h:c:", ["court="])
+    except getopt.GetoptError as o:
+        print(o)
+        print('name.py -c <court> ')
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt == '-h':
+            print('name.py -u <court>')
+            sys.exit()
+        elif opt in ("-c", "--court"):
+            court = str(arg)
+    if court is None:
+        print('please enter your court!')
+        sys.exit()
+
     try:
         instance = Parser()
+        instance.court_name = court
         our_data = instance.soup_flow()
-        instance.save_csv(our_data)
+        instance.save_csv(our_data, court)
         if instance.data:
             print('Parsing has been Success!')
         else:
@@ -88,3 +126,8 @@ if __name__ == '__main__':
         print(ex)
     finally:
         instance.driver.quit()
+
+
+
+if __name__ == '__main__':
+    main(sys.argv[1:])
